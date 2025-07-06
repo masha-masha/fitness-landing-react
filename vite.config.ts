b30b1846fbc6a673b97b77e5e56ae84800e1138b
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-07-06 22:32:09 +0300

    add base into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: [{find: "@", replacement: path.resolve(process.cwd(), "src")}]
   }
